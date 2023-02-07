35e79ce3524c273a638afc5973b7917423e7d6d5
jaohvi <vicentelegal@hotmail.com>
2023-02-06 21:59:44 -0400

    Adicionando main.py

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ print("***************")
 print("BEM VINDO PARA ADIVINHAR")
 print("***************")
 numero=65
-chute_str=input("Digite o seu número.:")
-print("Você digitou:", chute_str)
-chute=int(chute_str)
-x= numero-chute
 tentativas=3
 #rodada=1
 #while(rodada <= tentativas):
 for (rodada) in range(1,tentativas+1):
     print("Tentativa {} de {}".format(rodada, tentativas))
+    chute_str = input("Digite um número entre 0 e 100:")
+    chute = int(chute_str)
+    print("Você digitou:", chute_str)
+
+    if(chute<1 or chute >100):
+        print("Digite um valor entre 0 e 100!")
+        continue
+    x = numero - chute
     if (x==0):
         print("Parabéns, você acertou!!")
         print("Fim de jogo.")
@@ -22,8 +26,8 @@ for (rodada) in range(1,tentativas+1):
         else:
                 print("O seu chute foi menor que o numero secreto")
 
-        chute_str = input("Digite o seu número:")
-        print("Você digitou:", chute_str)
-        chute = int(chute_str)
-        x = numero - chute
+        #chute_str = input("Digite o seu número:")
+      #  print("Você digitou:", chute_str)
+      #  chute = int(chute_str)
+       # x = numero - chute
        # rodada= rodada + 1
