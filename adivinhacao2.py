32b09d44eb4a7c872558c5be6a870268075f36ec
jaohvi <vicentelegal@hotmail.com>
2023-02-12 18:32:52 -0400

    Adicionando main.py

--- adivinhacao2.py
+++ adivinhacao2.py
@@ -2,3 +2,6 @@ def jogar_adivinhacao2():
     print("Bem vindo a adivinhação 2")
 if(__name__=="__main__"):
     jogar_adivinhacao2()
+    raio = int(input("Valor do raio: "))
+    A = 3.14159 * raio
+    print("O valor do raio é {}".format(A))
